1d506fa63b1c4ee62187652b283548f73f2659a1
Gabriel Tauber <gabri.tauber@gmail.com>
2021-02-01 09:14:56 -0300

    Imp. Get-Post Theme

--- src/app/theme/theme.component.ts
+++ src/app/theme/theme.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Theme } from '../model/Theme';
+import { ThemeService } from '../service/theme.service';
 
 @Component({
   selector: 'app-theme',
@@ -8,17 +10,36 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./theme.component.css']
 })
 export class ThemeComponent implements OnInit {
+
+  theme: Theme = new Theme()
+  listThemes: Theme[]
   
   constructor(
-    private router: Router
+    private router: Router,
+    private themeService: ThemeService
   ) { }
 
   ngOnInit() {
     if (environment.token == '') {
       // alert ('You session is over, login again.')
       this.router.navigate(['/enter'])
-   }
-   
+    }
+
+    this.findAllThemas()
+  }
+
+  findAllThemas(){
+    this.themeService.getAllTheme().subscribe((resp: Theme[]) => {
+      this.listThemes = resp
+    })
+  }
+
+  register(){
+    this.themeService.postTheme(this.theme).subscribe((resp: Theme)=>{
+      this.theme = resp
+      alert('Theme created!')
+      this.theme = new Theme()
+    })
   }
 
 }
